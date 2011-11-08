ca38bc37f636c8ac576f6b6f2cea9728e3b051ae
Ryan Myers <ryan.p.myers@gmail.com>
2011-11-08 15:00:01 -0500

    First attemtp at creating a PDF. Just a simple Hello world for now.

---
+++ survey-appendix-g.php
@@ -0,0 +1,36 @@
+<?php
+require_once('tcpdf/tcpdf.php');
+
+// create new PDF document
+$pdf = new TCPDF();
+
+// set document information
+
+$pdf->SetCreator('Survey Plugin');
+$pdf->SetAuthor('Reterborough FHT');
+$pdf->SetTitle('Youth Sports Concussion Program Return to Play Clearance Form');
+$pdf->SetSubject('Youth Sports');
+$pdf->SetKeywords('Appendix G');
+
+// remove default header/footer
+$pdf->setPrintHeader(false);
+$pdf->setPrintFooter(false);
+
+// ---------------------------------------------------------
+
+// add a page
+$pdf->AddPage();
+
+// create some HTML content
+$html = '<div style"color:#FF0000">Hello World</div>';
+
+// output the HTML content
+$pdf->writeHTML($html);
+
+// ---------------------------------------------------------
+//Close and output PDF document
+//$pdf->Output('Return-To-Play.pdf', 'D'); // Force Download
+$pdf->Output('Return-To-Play.pdf', 'I');
+//============================================================+
+// END OF FILE                                                
+//============================================================+
