7f770aae3dae7b15d6a84fcd8f30092b3b17a616
Ryan Myers <ryan.p.myers@gmail.com>
2011-12-10 20:53:09 -0500

    Added a new library to handle importing existing PDFs for editing with tcpdf. Also created an example in test.php

--- survey-appendix-h.php
+++ survey-appendix-h.php
@@ -24,67 +24,58 @@ $pdf->AddPage();
 // create some HTML content
 $html = <<<HTML
 <center>
-<table style="width: 800px;" cellpadding="0" cellspacing="0">
-  <tbody>
-    <tr>
-      <td><img style="width: 800px; height: 119px;" alt=""
- src="images/apH.PNG"> </td>
-    </tr>
-    <tr>
-      <td style="font-family: Arial;" align="left">
-      <table>
-        <tbody>
-          <tr>
-            <td> Patient Name: _____________________________________</td>
-            <td> Date of Visit: _____________ </td>
-          </tr>
-          <tr>
-            <td> <br>
-School: _____________________________________ </td>
-            <td> <br>
-Date of Birth: _____________ </td>
-          </tr>
-        </tbody>
-      </table>
-      <br>
-&nbsp;The patient above has been have been diagnosed with a concussion
-and is not considered safe to return to collision sport activity until
-cleared at an appropriate time by a physician or nurse practitioner.
-The following text outlines current activity restrictions: <br>
-      <br>
-&nbsp;No physical exertion until scheduled reassessment on
-_________________. <br>
-      <br>
-Physical exertion is permitted with the following restrictions as of
-_________________: <br>
-___ Light non-contact exertion.<br>
-&nbsp;___ Moderate non-contact exertion. <br>
-___ Heavy non-contact exertion. <br>
-      <br>
-Physical exertion is permitted when symptom free for _____ days with
-the following restrictions as of _________________: <br>
-___ Light non-contact exertion. <br>
-___ Moderate non-contact exertion. <br>
-___ Heavy non-contact exertion.&nbsp; <br>
-      <br>
-Begin Youth Sports Concussion Program Return to Play Protocol. <br>
-      <br>
-Signature: _____________________________________________ <br>
-      <br>
-ACTIVITY LEVELS <br>
-Light non-contact exertion: Walking, stationary cycling, light jogging,
-light resistance training <br>
-Moderate non-contact exertion: Moderate jogging, stationary cycling,
-elliptical, moderate resistance training <br>
-Heavy non-contact exertion: Sprinting, heavy resistance training,
-non-contact sport drills, plyometrics <br>
-      <br>
-*If development of worsening of symptoms occurs with recommended
-activity levels/ restrictions then please discontinue activity for 24
-hours and resume at a lighter level.</td>
-    </tr>
-    <tr>
-      <td align="left"> &nbsp;</td>
+<table cellpadding="0" cellspacing="0">
+    <tbody>
+        <tr><td><img alt="Appendix H" src="images/apH.PNG"></td></tr>
+        <tr>
+        <td align="left">
+            <table>
+                <tbody>
+                    <tr>
+                        <td>Patient Name: __________________________</td>
+                        <td>Date of Visit: _______</td>
+                    </tr>
+                    <tr>
+                        <td>School: _____________</td>
+                        <td>Date of Birth: _____________</td>
+                    </tr>
+                </tbody>
+            </table>
+            <br>
+            The patient above has been have been diagnosed with a concussion
+            and is not considered safe to return to collision sport activity until
+            cleared at an appropriate time by a physician or nurse practitioner.
+            The following text outlines current activity restrictions: <br>
+            <br>
+            No physical exertion until scheduled reassessment on _________________. <br>
+            <br>
+            Physical exertion is permitted with the following restrictions as of _________________:<br>
+            <br>
+            ___ Light non-contact exertion.<br>
+            ___ Moderate non-contact exertion. <br>
+            ___ Heavy non-contact exertion. <br>
+            <br>
+            <br>
+            Physical exertion is permitted when symptom free for _____ days <br>
+            with the following restrictions as of _________________: <br>
+            <br>
+            ___ Light non-contact exertion. <br>
+            ___ Moderate non-contact exertion. <br>
+            ___ Heavy non-contact exertion <br>
+            <br>
+            Begin Youth Sports Concussion Program Return to Play Protocol. <br>
+            <br>
+            Signature: _____________________________________________ <br>
+            <br>
+            ACTIVITY LEVELS <br>
+            Light non-contact exertion: Walking, stationary cycling, light jogging, light resistance training <br>
+            Moderate non-contact exertion: Moderate jogging, stationary cycling, elliptical, 
+            moderate resistance training <br>
+            Heavy non-contact exertion: Sprinting, heavy resistance training, non-contact sport drills, plyometrics <br>
+            <br>
+            *If development of worsening of symptoms occurs with recommended activity levels/ restrictions then 
+            please discontinue activity for 24 hours and resume at a lighter level.
+        </td>
     </tr>
   </tbody>
 </table>
@@ -98,7 +89,9 @@ $pdf->writeHTML($html);
 //Close and output PDF document
 //$pdf->Output('Return-To-Play.pdf', 'D'); // Force Download
 //$pdf->Output('Return-To-Play.pdf', 'I'); //Output to screen.
-$pdf->Output(sys_get_temp_dir().'/appendix-h.pdf', 'F'); //Save file
+//$pdf->Output(sys_get_temp_dir().'/appendix-h.pdf', 'F'); //Save file
+$pdf->Output(sys_get_temp_dir().'/appendix-h.pdf', 'FI'); //Output to screen. and save to location.
+//$pdf->Output(sys_get_temp_dir().'/appendix-h.pdf', 'FD'); //Force Download. and save to location.
 //============================================================+
 // END OF FILE                                                
 //============================================================+
