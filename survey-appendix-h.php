9be90525c6af09036e1a3d5f2e27c2739c44e074
Ryan Myers <ryan.p.myers@gmail.com>
2011-12-15 08:29:04 -0500

    Added in the Basic parts of the appendix h.

--- survey-appendix-h.php
+++ survey-appendix-h.php
@@ -28,6 +28,9 @@ global $wpdb;
 $query = "SELECT fullname FROM {$wpdb->prefix}survey_users WHERE id=%d";
 $patient_name = $wpdb->get_var($wpdb->prepare($query, $_POST['user']));
 
+$query = "SELECT answer FROM {$wpdb->preix}survey_user_answers WHERE user=%d AND question=59";
+$school = $wpdb->get_var($wpdb->prepare($query, $_POST['user']));
+
 // initiate PDF
 $pdf = new PDF();
 $pdf->SetMargins(40, 48, PDF_MARGIN_RIGHT);
@@ -42,8 +45,25 @@ $pdf->AddPage();
 //http://www.tcpdf.org/doc/classTCPDF.html#a8458280d15b73d3baffb28eebe2d9246
 
 //The gist of it is this though: (width of cell, line height, top left x co-ordinate, tl Y co-ord, string to oputput)
+//
 $pdf->writeHTMLCell(50, 1, 40, 48, $patient_name);
-$pdf->writeHTMLCell(50, 1, 150, 48, "Start Date");
+$pdf->writeHTMLCell(50, 1, 150, 48, "Start TEST Date");
+$pdf->writeHTMLCell(50, 1, 30, 58, "Start TEST School");
+$pdf->writeHTMLCell(50, 1, 150, 58, "Date of Birth");
+$pdf->writeHTMLCell(50, 1, 26, 86, "x");
+$pdf->writeHTMLCell(50, 1, 125, 86, "date Reassessment");
+$pdf->writeHTMLCell(50, 1, 26, 101, "x");
+$pdf->writeHTMLCell(50, 1, 143, 101, "date Restrictions");
+$pdf->writeHTMLCell(50, 1, 45, 108, "x");
+$pdf->writeHTMLCell(50, 1, 45, 115, "x");
+$pdf->writeHTMLCell(50, 1, 45, 122, "x");
+$pdf->writeHTMLCell(50, 1, 26, 137, "x");
+$pdf->writeHTMLCell(50, 1, 125, 137, "4");
+$pdf->writeHTMLCell(50, 1, 65, 142, "restrictions");
+$pdf->writeHTMLCell(50, 1, 45, 149, "x");
+$pdf->writeHTMLCell(50, 1, 45, 156, "x");
+$pdf->writeHTMLCell(50, 1, 45, 163, "x");
+$pdf->writeHTMLCell(50, 1, 26, 178, "x");
 
 // ---------------------------------------------------------
 //Close and output PDF document
